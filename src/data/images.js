2c021813675764e3f9cbd73eb81b58c65c11239b
jameswquinn <jameswquinn@users.noreply.github.com>
2024-07-24 18:32:04 +0100

    Create images.js

---
+++ src/data/images.js
@@ -0,0 +1,12 @@
+export const images = [
+  {
+    src: 'https://via.placeholder.com/600x400',
+    srcSet: 'https://via.placeholder.com/400x267 400w, https://via.placeholder.com/600x400 600w',
+    sizes: '(max-width: 600px) 100vw, 600px',
+    alt: 'Placeholder image 1',
+    placeholder: 'https://via.placeholder.com/10',
+    width: '600',
+    height: '400',
+  },
+  // Add more image objects here
+];
