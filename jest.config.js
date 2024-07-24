05cb69ee9bedb07f113bd342057aaeb41a61ce88
jameswquinn <jameswquinn@users.noreply.github.com>
2024-07-24 18:36:03 +0100

    Create jest.config.js

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  preset: 'jest-preset-preact',
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+  },
+};
